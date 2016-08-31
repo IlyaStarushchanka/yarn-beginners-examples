93d125425b604ebead1dd6f56fb1925ec4f52012
Ilya_Starushchanka <ilya10194@mail.ru>
2016-08-31 17:42:57 +0300

    Doing logic

--- src/main/java/com/wikibooks/hadoop/yarn/examples/HelloYarn.java
+++ src/main/java/com/wikibooks/hadoop/yarn/examples/HelloYarn.java
@@ -30,13 +30,103 @@ public class HelloYarn {
   }
 
   public void printMemoryStats() {
-    long freeMemory = bytesToMegabytes(Runtime.getRuntime().freeMemory());
+    /*long freeMemory = bytesToMegabytes(Runtime.getRuntime().freeMemory());
     long totalMemory = bytesToMegabytes(Runtime.getRuntime().totalMemory());
     long maxMemory = bytesToMegabytes(Runtime.getRuntime().maxMemory());
 
     System.out.println("The amount of free memory in the Java Virtual Machine: " + freeMemory);
     System.out.println("The total amount of memory in the Java virtual machine: " + totalMemory);
-    System.out.println("The maximum amount of memory that the Java virtual machine: " + maxMemory);
+    System.out.println("The maximum amount of memory that the Java virtual machine: " + maxMemory);*/
+    try{
+      Pattern p = Pattern.compile("http[s]*:[^\\s\\r\\n]+");
+      List<String> urls = new ArrayList<String>();
+
+      Path pt=new Path(Constants.INPUT_FILE);
+      FileSystem fs = FileSystem.get(new Configuration());
+      BufferedReader br=new BufferedReader(new InputStreamReader(fs.open(pt)));
+      List<String> lines = new ArrayList<String>();
+
+      String line=br.readLine();
+      String topLine = line;
+      line=br.readLine();
+      while (line != null){
+        lines.add(line.trim());
+        line=br.readLine();
+      }
+
+      System.out.println("STEP 1 " + lines.size());
+      for (String l : lines) {
+        Matcher m = p.matcher(l);
+        m.matches();
+        while (m.find()) {
+          urls.add(m.group());
+        }
+      }
+      //List<String> urls = getUrlsFromDB();
+      System.out.println("STEP 2 " +urls.size());
+      List<List<String>> totalTopWords = new ArrayList<>();
+      for (String u : urls) {
+        Document d = Jsoup.connect(u).get();
+        String text = d.body().text();
+
+        StringTokenizer tokenizer = new StringTokenizer(text, " .,?!:;()<>[]\b\t\n\f\r\"\'\\");
+        List<String> words = new ArrayList<String>();
+        while(tokenizer.hasMoreTokens()) {
+          words.add(tokenizer.nextToken());
+//System.out.println(tokenizer.nextToken());
+        }
+
+        List<String> topWords = words.stream()
+                .map(String::toLowerCase)
+                .collect(groupingBy(Function.identity(), counting()))
+                .entrySet().stream()
+                .sorted(Map.Entry.<String, Long> comparingByValue(reverseOrder()).thenComparing(Map.Entry.comparingByKey()))
+                .limit(10)
+                .map(Map.Entry::getKey)
+                .collect(toList());
+        totalTopWords.add(topWords);
+      }
+
+      System.out.println("STEP 3 " +totalTopWords.size());
+      try{
+        Path ptOut=new Path(Constants.OUTPUT_FILE);
+        FileSystem fsOut = FileSystem.get(new Configuration());
+        BufferedWriter brOut = new BufferedWriter(new OutputStreamWriter(fsOut.create(ptOut,true)));
+
+        brOut.write(topLine);
+        brOut.write("\n");
+        System.out.println("STEP 4");
+        for (int i = 0; i < lines.size(); i++) {
+          String currentLine = lines.get(i);
+          String[] params = currentLine.split("\\s+");
+          for (int j = 0; j < params.length; j++) {
+            if (j == 1) {
+              List<String> currentTopWords = totalTopWords.get(i);
+
+              for (int k = 0; k < currentTopWords.size(); k++) {
+                brOut.write(currentTopWords.get(k));
+                if (k < (currentTopWords.size()-1)) {
+                  brOut.write(",");
+                }
+              }
+              brOut.write(" ");
+            }
+            brOut.write(params[j]);
+            if (j < (params.length-1)) {
+              brOut.write(" ");
+            }
+          }
+          brOut.write("\n");
+        }
+
+        System.out.println("STEP 5");
+        brOut.close();
+      }catch(Exception e) {
+        System.out.println(e.getMessage());
+      }
+    }catch(Exception e){
+      System.out.println(e.getMessage());
+    }
   }
 
   public static void main(String[] args) {
